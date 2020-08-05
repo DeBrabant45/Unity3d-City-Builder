5fbf6b527a0663249e84dcf5e9422a989a37f903
Aaron <DeBrabant45@users.noreply.github.com>
2020-08-05 13:51:45 -0400

    Fixing multi-upgrade bug

--- Assets/Scripts/BuildingManagerHelpers/StructureUpgradeHelper.cs
+++ Assets/Scripts/BuildingManagerHelpers/StructureUpgradeHelper.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class StructureUpgradeHelper : StructureModificationHelper
 {
     private Dictionary<Vector3Int, GameObject> _oldStructuresBeforeUpgrade = new Dictionary<Vector3Int, GameObject>();
+    private Dictionary<Vector3Int, StructureBaseSO> _newStructureData = new Dictionary<Vector3Int, StructureBaseSO>();
 
     public StructureUpgradeHelper(StructureRepository structureRepository, GridStructure grid, IPlacementManager placementManager, IResourceManager resourceManager) 
         : base(structureRepository, grid, placementManager, resourceManager)
@@ -23,16 +24,28 @@ public class StructureUpgradeHelper : StructureModificationHelper
 
     public override void ConfirmModifications()
     {
-
-        foreach (var gridPosition in _structuresToBeModified.Keys)
-        {
-            PrepareStructureForUpgrade(gridPosition);
-        }
         DestroyOldStructuresForUpgrade();
-        base.ConfirmModifications();
+        PlaceUpgradedStructuresOnTheMap();
         _structuresToBeModified.Clear();
     }
 
+    private void PlaceUpgradedStructuresOnTheMap()
+    {
+        _placementManager.PlaceStructuresOnTheMap(_structuresToBeModified.Values);
+        Type structureType = _structureData.GetType();
+        foreach (var keyValuePair in _structuresToBeModified)
+        {
+            PrepareStructureForUpgrade(keyValuePair.Key);
+            foreach (var structure in _newStructureData)
+            {
+                if (keyValuePair.Key == structure.Key)
+                {
+                    _grid.PlaceStructureOnTheGrid(keyValuePair.Value, keyValuePair.Key, GameObject.Instantiate(structure.Value));
+                }
+            }
+            StructureEconomyManager.CheckStructureTypeForCreationPreparation(structureType, keyValuePair.Key, _grid);
+        }
+    }
 
     private void PrepareStructureForUpgrade(Vector3Int gridPosition)
     {
@@ -46,7 +59,6 @@ public class StructureUpgradeHelper : StructureModificationHelper
             }
             data.upgradeActive = true;
             data.SetUpgradedIncome(GetStructureUpgradeIncome(data));
-            StructureEconomyManager.CheckStructureTypeForCreationPreparation(dataType, gridPosition, _grid);
         }
     }
 
@@ -56,19 +68,19 @@ public class StructureUpgradeHelper : StructureModificationHelper
 
         if (_grid.IsCellTaken(gridPosition) == true && _grid.GetStructureDataFromTheGrid(inputPosition).upgradable == true)
         {
-            _structureData = _grid.GetStructureDataFromTheGrid(gridPosition);
+            var structureBase = _grid.GetStructureDataFromTheGrid(gridPosition);
             var structure = _grid.GetStructureFromTheGrid(gridPosition);
             var gridPositionInt = Vector3Int.FloorToInt(gridPosition);
             if (_structuresToBeModified.ContainsKey(gridPositionInt))
             {
                 RevokeStructureUpgradePlacementAt(gridPositionInt, structure);
-                _resourceManager.AddMoneyAmount(_structureData.upgradePlacementCost);
+                _resourceManager.AddMoneyAmount(structureBase.upgradePlacementCost);
             } 
-            else if (_resourceManager.CanIBuyIt(_structureData.upgradePlacementCost))
+            else if (_resourceManager.CanIBuyIt(structureBase.upgradePlacementCost))
             {
                 AddOldStructureForUpgrade(gridPositionInt, structure);
-                PlaceUpgradedStructureAt(gridPosition, gridPositionInt);
-                _resourceManager.SpendMoney(_structureData.upgradePlacementCost);
+                PlaceUpgradedStructureAt(gridPosition, gridPositionInt, structureBase);
+                _resourceManager.SpendMoney(structureBase.upgradePlacementCost);
             }
 
         }
@@ -81,6 +93,7 @@ public class StructureUpgradeHelper : StructureModificationHelper
         _placementManager.DestroySingleStructure(upGradedStructure);
         _structuresToBeModified.Remove(gridPositionInt);
         _oldStructuresBeforeUpgrade.Remove(gridPositionInt);
+        _newStructureData.Remove(gridPositionInt);
         structure.SetActive(true);
     }
 
@@ -113,17 +126,17 @@ public class StructureUpgradeHelper : StructureModificationHelper
         _oldStructuresBeforeUpgrade.Clear();
     }
 
-    private void PlaceUpgradedStructureAt(Vector3 gridPosition, Vector3Int gridPositionInt)
+    private void PlaceUpgradedStructureAt(Vector3 gridPosition, Vector3Int gridPositionInt, StructureBaseSO structureBase)
     {
         GameObject buildingPrefab = null;
         foreach (var structure in _structureRepository.modelDataCollection.zoneStructures)
         {
             if(_grid.GetStructureDataFromTheGrid(gridPosition).buildingName == structure.buildingName)
             {
-                buildingPrefab = structure.upgradePrefabVariants[0];                
+                buildingPrefab = structure.upgradePrefabVariants[0];
             }
         }
-
+        _newStructureData.Add(gridPositionInt, structureBase);
         _structuresToBeModified.Add(gridPositionInt, _placementManager.CreateGhostStructure(gridPosition, buildingPrefab));
     }
 
