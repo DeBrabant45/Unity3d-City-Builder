7fbdc9969c4fa91d0e8759e8c9eb8aa225291af5
Aaron <DeBrabant45@users.noreply.github.com>
2020-08-08 16:17:20 -0400

    Adding start menu with levelmanger and level fading

---
+++ Assets/Scripts/SceneScripts/LevelFade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelFade : MonoBehaviour
+{
+    public float fadeInTime;
+
+    private Image fadePanel;
+    private Color currentColor = Color.black;
+
+    // Use this for initialization
+    void Start()
+    {
+        fadePanel = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad < fadeInTime)
+        {
+            //Fade in
+            float alphaChange = Time.deltaTime / fadeInTime;
+            //taking the alpha channel off and reduce by alphaChange
+            currentColor.a -= alphaChange;
+            //Now set the fade color
+            fadePanel.color = currentColor;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneScripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(int levelIndex)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
