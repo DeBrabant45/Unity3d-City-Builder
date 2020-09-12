eb2d0be231b24c735a805b8fd853c6774803fba5
Aaron <DeBrabant45@users.noreply.github.com>
2020-09-11 22:18:15 -0400

    Adding material build timer

--- Assets/Scripts/ScriptableObjects/ManufacturerBaseSO.cs
+++ Assets/Scripts/ScriptableObjects/ManufacturerBaseSO.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Manufactory", menuName = "CityBuilder/StructureData/Manufactory")]
@@ -7,9 +9,29 @@ public class ManufacturerBaseSO : StructureBaseSO
 {
     [SerializeField]
     private int materialAmount;
+    [SerializeField]
+    private int materialBuildTimer;
+    private Stopwatch stopwatch = new Stopwatch();
 
     public int GetMaterialAmount()
     {
-        return materialAmount;
+        if(MaterialPlaceTimer() == true)
+        {
+            return materialAmount;
+        }
+        return 0;
+    }
+
+    private bool MaterialPlaceTimer()
+    {
+        stopwatch.Start();
+        while (stopwatch.Elapsed >= TimeSpan.FromSeconds(materialBuildTimer))
+        {
+            stopwatch.Stop();
+            stopwatch.Reset();
+            return true;
+        }
+
+        return false;
     }
 }
