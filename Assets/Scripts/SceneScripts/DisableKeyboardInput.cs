540638949b70c91568d0111a824b241fa33e7234
Aaron <DeBrabant45@users.noreply.github.com>
2020-11-18 15:29:09 -0500

    Fixing bug with web GL
    Unity Web GL is capturing all keyboard inputs which made the feedback input fields unable to use.

---
+++ Assets/Scripts/SceneScripts/DisableKeyboardInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableKeyboardInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.WebGLInput.captureAllKeyboardInput = false;
+    }
+}
